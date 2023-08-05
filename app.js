07d51c7d57ac1e4c7057c31cca9da674853add6e
codingGanster <133964102+codingGanster@users.noreply.github.com>
2023-08-05 14:13:54 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function colour(){
+    const body = document.getElementById('body1');
+    body.style.backgroundColor='#000';
+}
+
+function defaultC(){
+    const body = document.getElementById('body1');
+    body.style.backgroundColor='plum';
+}
